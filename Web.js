7a343cfbbbbff476b29a5563f36df1bb4612dd5c
kwonjs9804 <kwonjs9804@naver.com>
2023-06-08 16:57:50 +0900

    edit js

--- Web.js
+++ Web.js
@@ -404,9 +404,10 @@ const positions = [
     },
 ];
 
+const infowindowList = [];
+
 for (let i = 0; i < positions.length; i ++) {
     // 마커를 생성합니다
-    let isOpen = false;
     const marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         removable: true,
@@ -417,17 +418,24 @@ for (let i = 0; i < positions.length; i ++) {
         content: positions[i].content, // 인포윈도우에 표시할 내용
     });
 
+    infowindowList.push(infowindow);
+
 
     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
     // 이벤트 리스너로는 클로저를 만들어 등록합니다 
     // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-    kakao.maps.event.addListener(marker, 'click', makeOverListener(map, marker, infowindow));
+    kakao.maps.event.addListener(marker, 'click', makeOverListener(map, marker, infowindow, i));
     kakao.maps.event.addListener(map, 'click', makeOutListener(infowindow));
 }
 // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
-function makeOverListener(map, marker, infowindow) {
+function makeOverListener(map, marker, infowindow, i) {
     return function() {
         infowindow.open(map, marker);
+        infowindowList.forEach((w, idx) => {
+            if (idx !== i) {
+                w.close();
+            }
+        })
     };
 }
 // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
